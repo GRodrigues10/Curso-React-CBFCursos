6f4f2f379d4a93a0be7cbdb045a977abe699b07a
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-02 16:47:14 -0300

    aula 9

--- src/Components/Relogio.jsx
+++ src/Components/Relogio.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 
-import React from "react";
 
 export default function Relogio(){
+
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+    useEffect(() => {
+        // Configura o intervalo para atualizar o tempo a cada segundo
+        const intervalId = setInterval(() => {
+          setTime(new Date().toLocaleTimeString());
+        }, 1000);
+    
+        // Limpa o intervalo quando o componente for desmontado
+        return () => clearInterval(intervalId);
+      }, []);
+
+
     return(<>
     
             <p>
-                {new Date().toLocaleTimeString()}
+                {time}
             </p>
         
            </>)
