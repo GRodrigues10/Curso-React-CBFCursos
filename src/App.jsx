3c38dec0f1416f81a22ecf97bf3251305a459962
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-02 16:14:56 -0300

    Aula 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
+import './App.css';
 
 function App() {
+
+  const textoDestaque = {
+    color:'blue',
+    fontSize: '3.5rem'
+  }
+
   return (
     <>
-      <Header />
-      <Body />
+
+      {/* <Header />
+      <Body /> */}
+      <section className="caixa">
+        <h1 style={{color:'red', fontSize:'5rem'}}>CBF Cursos</h1>
+        <h2 style={textoDestaque}>Curso de React</h2>
+        <p className="texto">Hello There!</p>
+        <a href="https://youtube.com/cfbcursos" target="_blank">Curso</a>
+      </section>
+
     </>
   );
 }
