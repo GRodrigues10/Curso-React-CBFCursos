1e2cbb014d2a661c636c0b616df11e98d468a3dd
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-02 16:35:55 -0300

    aula 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
+
 import './App.css';
+import Relogio from "./Components/Relogio";
 
 function App() {
 
@@ -12,14 +14,17 @@ function App() {
 
   return (
     <>
-
+      
       {/* <Header />
       <Body /> */}
       <section className="caixa">
+      <Relogio/>
+      
         <h1 style={{color:'red', fontSize:'5rem'}}>CBF Cursos</h1>
         <h2 style={textoDestaque}>Curso de React</h2>
         <p className="texto">Hello There!</p>
         <a href="https://youtube.com/cfbcursos" target="_blank">Curso</a>
+        
       </section>
 
     </>
