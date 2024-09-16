d79e9f394fad29510d7894fbab0e5418120a85bf
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-16 12:03:48 -0300

    aula 19

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import React, { useState } from "react";
-import Nota from "./Components/Nota";
-import Resultado from "./Components/Resultado";
-import Caixa from "./Components/Caixa";
-import Canal from "./Components/Canal";
+import React, { useState, useEffect } from "react";
 
 function App() {
+    // Função para incrementar o contador
+    const incrementCount = () => {
+        console.log("Botão clicado, atualizando contagem");
+        setContagem(c => c + 1);
+    };
 
+    const [contagem, setContagem] = useState(0);
 
+   
+    useEffect(() => {
+        console.log('Página Carregada!');
+        document.title = `Contagem ${contagem}`;
+    }); 
 
-  return (
-    <>
-    <Caixa site= 'www.cfbcursos.com.br'>
-      <Canal/>
-        <h1>CBF Cursos</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit esse ex nostrum cumque, temporibus inventore aut provident debitis eaque dicta aliquam voluptatem magni sunt dolorem quod quibusdam tempora iste magnam tempore? Animi aperiam eligendi consequatur minus! Dolor sint voluptatum obcaecati, placeat quibusdam ex necessitatibus provident eos alias. Dignissimos facilis, unde hic ut vero facere. Explicabo repellat quo necessitatibus dolores ut perspiciatis consectetur ab enim dolorum consequatur, laborum laudantium molestiae. Ex exercitationem reprehenderit totam ducimus accusamus, maxime eius iste dolorum necessitatibus labore quaerat delectus, perspiciatis sint minima dolore blanditiis quam harum magnam obcaecati omnis est praesentium quae voluptate vel. Amet, illum.</p>
-    </Caixa>
-      
-    </>
+    return (
+      <>
+          <p>Contagem: {contagem}</p>
+          <button onClick={incrementCount}>Contar</button>
+
+      </>
   );
 }
 
