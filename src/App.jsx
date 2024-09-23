22b1fe8d81650b3af0fb5550eb7b96f9ba3ad4f9
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-09-23 16:19:30 -0300

    aula 21

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,10 @@ import React, { useState, useEffect } from "react";
 
 function App() {
    
-    const [nome, setNome] = useState();
-
-    const armazenar = (chave, valor) => {
-        localStorage.setItem(chave, valor)
-    }
-    const consultar = (chave) => {
-        alert(localStorage.getItem(chave))
-    }
-    const apagar = (chave) => {
-        localStorage.removeItem(chave)
-    }
 
     return (
       <>
-         
-        <label>Digite um Nome</label> <br />
-        <input type="text" value={nome} onChange={(e)=>setNome(e.target.value)}/> <br />
-        <button onClick={()=>armazenar('ls_nome', nome)}>Gravar Nome</button>
-        <button onClick={()=>consultar('ls_nome')}>Ver Nome</button>
-        <button onClick={()=>apagar('ls_nome')}>Remover Nome</button>
-
+       <h1>Componentes de Classe</h1>
       </>
   );
 }
